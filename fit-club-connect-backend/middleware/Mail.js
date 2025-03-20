7cc11d9a7fcfd95c9b16286ef1f5d38e19b27a78
laksh-wadhwani <laksh.wadhwani55@gmail.com>
2025-03-20 17:30:29 +0500

    Update Mail.js

--- fit-club-connect-backend/middleware/Mail.js
+++ fit-club-connect-backend/middleware/Mail.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer')
 let transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false,
+    secure: true,
     auth: {
         user: process.env.SMTP_MAIL,
         pass: process.env.SMTP_PASSWORD,
